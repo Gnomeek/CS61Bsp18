9f049042f161283b5fea70ba9108fa03859b00d0
gnomeek <gnomeek@outlook.com>
2019-07-04 20:32:40 +0800

    bug handle

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -15,98 +15,11 @@ public class ArrayDeque<T> {
         usage = 0;
     }
 
-    /** add the item to the first of the ArrayDeque */
-    public void addFirst(T i) {
-        resize();
-        this.items[nextFirst] = i;
-        if (nextFirst == 0) {
-            nextFirst = items.length - 1;
-        } else {
-            nextFirst -= 1;
-        }
-        size += 1;
-    }
-
-    /** add the item to the last of the ArrayDeque */
-    public void addLast(T i) {
-        resize();
-        this.items[nextLast] = i;
-
-        if (nextLast == items.length - 1) {
-            nextLast = 0;
-        } else {
-            nextLast += 1;
-        }
-        size += 1;
-    }
-
-    /** return true if the ArrayDeque is empty, false otherwise */
-    public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    /** return the size of the ArrayDeque */
-    public int size() {
-        return size;
-    }
-
-    /** print the items in the ArrayDeque from the first to last, separated by a space */
-    public void printDeque() {
-        for (T p : items) {
-            System.out.print(p + " ");
-        }
-        System.out.print("\n");
-    }
-
-    /** remove the first item in the ArrayDeque and return it */
-    public T removeFirst() {
-        T temp = this.items[nextFirst + 1];
-        this.items[nextFirst + 1] = null;
-        if (size == 0) {
-            return null;
-        }
-
-        if (nextFirst == items.length - 1) {
-            temp = this.items[0];
-            this.items[0] = null;
-        }
-
-        size -= 1;
-        resize();
-        return temp;
-    }
-
-    /** remove the last item in the ArrayDeque and return it */
-    public T removeLast() {
-        T temp = this.items[nextLast - 1];
-        this.items[nextLast - 1] = null;
-        if (size == 0) {
-            return null;
-        }
-
-        if (nextLast == 0) {
-            temp = this.items[items.length - 1];
-            this.items[items.length - 1] = null;
-        }
-
-        size -= 1;
-        resize();
-        return temp;
-    }
-
-    /** get the item at the given index and return it */
-    public T get(int index) {
-        return this.items[index];
-    }
-
-    public boolean checkUsage() {
+    private boolean checkUsage() {
         return (usage < 0.25 && items.length > 16);
     }
 
-    public void resize() {
+    private void resize() {
         if (checkUsage() || size == items.length) {
             int targetSize = items.length;
 
@@ -145,5 +58,104 @@ public class ArrayDeque<T> {
                 nextLast = maxIndex - startIndex + 1;
             }
         }
+    }
+
+    /** add the item to the first of the ArrayDeque */
+    public void addFirst(T i) {
+        resize();
+
+        this.items[nextFirst] = i;
+        size += 1;
+
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst -= 1;
+        }
+
+        usage = (double)size / (double)items.length;
+    }
+
+    /** add the item to the last of the ArrayDeque */
+    public void addLast(T i) {
+        resize();
+
+        this.items[nextLast] = i;
+        size += 1;
+
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        } else {
+            nextLast += 1;
+        }
+
+        usage = (double)size / (double)items.length;
+    }
+
+    /** return true if the ArrayDeque is empty, false otherwise */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** return the size of the ArrayDeque */
+    public int size() {
+        return size;
+    }
+
+    /** print the items in the ArrayDeque from the first to last, separated by a space */
+    public void printDeque() {
+        for (T p : items) {
+            System.out.print(p + " ");
+        }
+        System.out.print("\n");
+    }
+
+    /** remove the first item in the ArrayDeque and return it */
+    public T removeFirst() {
+        T temp = this.items[nextFirst + 1];
+        this.items[nextFirst + 1] = null;
+        if (size == 0) {
+            return null;
+        }
+
+        if (nextFirst == items.length - 1) {
+            temp = this.items[0];
+            this.items[0] = null;
+        }
+
+        size -= 1;
+        usage = (double)size / (double)items.length;
+        resize();
+        return temp;
+    }
+
+    /** remove the last item in the ArrayDeque and return it */
+    public T removeLast() {
+        T temp = this.items[nextLast - 1];
+        this.items[nextLast - 1] = null;
+        if (size == 0) {
+            return null;
+        }
+
+        if (nextLast == 0) {
+            temp = this.items[items.length - 1];
+            this.items[items.length - 1] = null;
+        }
+
+        size -= 1;
+        usage = (double)size / (double)items.length;
+        resize();
+        return temp;
+    }
+
+    /** get the item at the given index and return it */
+    public T get(int index) {
+        if (index >= this.items.length) {
+            return null;
+        }
+        return this.items[index];
     }
 }
